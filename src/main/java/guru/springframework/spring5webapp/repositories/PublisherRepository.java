17bac5f9a1d020efd507f220b5ff6cedde8a89a3
smie8 <sami.eriksson@yahoo.fi>
2022-03-03 17:23:56 +0200

    Rest of the assignment #2

--- src/main/java/guru/springframework/spring5webapp/repositories/PublisherRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/PublisherRepository.java
@@ -1,2 +1,8 @@
-package guru.springframework.spring5webapp.repositories;public interface PublisherRepository {
+package guru.springframework.spring5webapp.repositories;
+
+import guru.springframework.spring5webapp.domain.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+
 }
